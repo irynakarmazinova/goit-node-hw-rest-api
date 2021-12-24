cdd8333404d7d4bb6eb6d9eec0ac50d0c0325718
irynakarmazinova <i.karmazinova1992@gmail.com>
2021-12-24 13:46:08 +0200

    add info

--- app.js
+++ app.js
@@ -12,12 +12,14 @@ const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
+// мидлвары
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json()); // мидлвар котор парсит json`a
 
 app.use("/api/contacts", contactsRouter);
 
+// порядок подключаемого промежуточного ПО имеет значение. В конце приложения идет обработка ошибок. Вначале происходит обработка несуществующего роута или ошибка 404
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
 });
