759244f6a5e8a997f94b5bb36a4bcdf46e8e51ad
irynakarmazinova <i.karmazinova1992@gmail.com>
2021-12-25 02:20:35 +0200

    add

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+// const mongoose = require("mongoose");
 
 require("dotenv").config();
 // const dotenv = require("dotenv");
@@ -28,5 +29,14 @@ app.use((err, req, res, next) => {
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
 });
+
+// const DB_HOST = "mongodb+srv://iryna:KrWV5bhaYxvzEm3d@cluster0.s32jl.mongodb.net/db-contacts?retryWrites=true&w=majority";
+// mongoose.connect(DB_HOST) .then(()=> {
+//     console.log("Database connection successful")
+// })
+// .catch(error => {
+//     console.log(error.message);
+//     process.exit(1);
+// })
 
 module.exports = app;
